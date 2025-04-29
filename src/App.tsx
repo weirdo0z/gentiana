164032dd04dbad6bf768daf495b925227fb2d48c
HideakiShirozu <hide_shiroozu@yahoo.co.jp>
2025-04-29 17:43:07 +0900

    feat: add legendstate

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import { observable } from "@legendapp/state"
+
+export const globalState$ = observable({})
+
 export default function App() {
   /** async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
