0318884638d787d98efd76d410b407e9f492c749
HideakiShirozu <hide_shiroozu@yahoo.co.jp>
2025-05-02 23:01:40 +0900

    feat: dat

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,6 +11,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_decorum::init())
         .setup(|app| {
             // Create a custom titlebar for main window
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import { observable } from "@legendapp/state"
+import { observable } from '@legendapp/state'
+import { LazyStore } from '@tauri-apps/plugin-store'
+import { useEffect } from 'preact/hooks'
 
-export const globalState$ = observable({})
+interface GlobalState {
+  page: 'main' | 'setting'
+}
+
+const initialGlobalState: GlobalState = {
+  page: 'main',
+}
+
+export const globalState$ = observable<GlobalState>(initialGlobalState)
 
 export default function App() {
   /** async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke('greet', { name }))
   } */
+
+  useEffect(() => {
+    const store = new LazyStore('setting.dat')
+
+    // Load the setting
+    ;(async () =>
+      globalState$.set(
+        (await store.get<GlobalState>('setting')) ?? initialGlobalState,
+      ))()
+
+    // Memorize what we were doing
+    const onExit = async () => {
+      await store.set('setting', globalState$.peek())
+    }
+
+    window.addEventListener('beforeunload', onExit)
+
+    return () => {
+      window.removeEventListener('beforeunload', onExit)
+    }
+  }, [])
 
   return (
     <main>
